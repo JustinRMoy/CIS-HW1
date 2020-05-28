6b0782aaeb9e2c5942af9a12e6c17caae6a9a08b
JustinRMoy <31043855+JustinRMoy@users.noreply.github.com>
2020-05-28 17:27:25 -0400

    Add files via upload
    HW1

---
+++ hw1.java
@@ -0,0 +1,205 @@
+
+
+import java.io.*;
+import java.util.Scanner;
+
+public class hw1 {
+
+    public static int[][] readKeyFile(File keyFile) throws IOException {
+
+        FileInputStream file = null;
+        int [][] kMatrix = new int[0][];
+        int i = 0;
+        int j = 0;
+
+        try {
+
+            file  = new FileInputStream(keyFile);
+            Scanner in = new Scanner(file);
+
+            int keySize = 0;
+
+            keySize = in.nextInt();
+
+            kMatrix = new int [keySize][keySize];
+
+            System.out.println("Key Matrix: \n");
+
+            while(in.hasNext()){
+                if(in.hasNextInt()) {
+                    if (j >= keySize) {
+                        j = 0;
+                        i++;
+                    }
+                    kMatrix[i][j] = in.nextInt();
+                    j++;
+                }else {
+                    in.next();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            file.close();
+        }
+
+
+        return kMatrix;
+    }
+
+    public static String readPlain (File pFile) throws  IOException{
+
+        FileInputStream file = null;
+
+        String plaintext = "";
+
+        try{
+
+            file = new FileInputStream(pFile);
+            Scanner in = new Scanner(file);
+
+            while (in.hasNext()) {
+                plaintext = in.nextLine();
+                //plaintext.concat("dog");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            file.close();
+        }
+
+        return plaintext;
+
+    }
+
+    public static void printTXT(String plainTxt){
+        int m =0;
+
+        for (int k =0; k < plainTxt.length(); k++) {
+
+            if(m >= 80){
+                m = 0;
+                System.out.println();
+            }
+
+            System.out.print(plainTxt.charAt(k));
+            m++;
+
+        }
+    }
+
+    public static void printKey(int [][] keyMatrix){
+        for(int i = 0; i < keyMatrix.length; i++){
+            for(int j = 0; j < keyMatrix.length; j++){
+                System.out.print(keyMatrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] convertToVector(String txt, int index, int keySize){
+
+        int[] ret = new int[keySize];
+
+        for(int i = 0; i < keySize; i++){
+
+            ret[i] = (int)(txt.charAt(index) - 'a');
+            index++;
+
+        }
+
+        return ret;
+
+    }
+
+    public static int dotProduct(int[] v1, int[]v2){
+
+        int ret = 0;
+
+        for(int i = 0; i < v1.length; i++){
+            ret += v1[i] * v2[i];
+        }
+
+        ret %= 26;
+
+        return ret;
+    }
+
+    public static String matrixMult(String plainTxt, int index, int[][] keyMatrix){
+
+        int[] vector = convertToVector(plainTxt, index, keyMatrix.length);
+
+        String res = "";
+
+        for(int row = 0; row < keyMatrix.length; row++){
+
+            res = res.concat(Character.toString((char)(dotProduct(keyMatrix[row], vector) + 'a')));
+
+        }
+
+        return res;
+    }
+
+    public static String hillCipher(String plainTxt, int [][] keyMatrix){
+
+        int keySize = keyMatrix.length;
+        String cipherTxt = "";
+
+        for(int i = 0; i < plainTxt.length(); i+= keySize){
+
+            cipherTxt = cipherTxt.concat(matrixMult(plainTxt, i, keyMatrix));
+
+        }
+
+        return cipherTxt;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        File in;
+        File plain;
+
+        String plainTxt = "";
+        String ciphertxt = "";
+
+        try{
+
+
+            in = new File(args[0]);
+
+
+            int [][] keyMatrix  = readKeyFile(in);
+
+            printKey(keyMatrix);
+
+            plain = new File(args[1]);
+
+            plainTxt = readPlain(plain);
+
+
+           plainTxt = plainTxt.replaceAll("[^a-zA-Z]", "");
+           plainTxt = plainTxt.toLowerCase();
+
+           while(plainTxt.length() % keyMatrix.length != 0){
+
+               plainTxt = plainTxt.concat("x");
+
+           }
+
+           System.out.println("\nPlaintext:\n");
+           printTXT(plainTxt);
+
+           ciphertxt = hillCipher(plainTxt, keyMatrix);
+
+           System.out.println("\n\n\nCiphertext:\n");
+           printTXT(ciphertxt);
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+        }
+    }
+
+}
